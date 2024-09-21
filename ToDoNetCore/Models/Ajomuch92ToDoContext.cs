b2961b7bec47cb3bd6b575feb52eaec7af442b50
ajomuch92 <aaronmontes_92@yahoo.es>
2024-09-21 15:57:39 -0600

    feat: 🎸 Remove connection string from file

--- ToDoNetCore/Models/Ajomuch92ToDoContext.cs
+++ ToDoNetCore/Models/Ajomuch92ToDoContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace ToDoNetCore.Models;
 
@@ -19,7 +20,7 @@ public partial class Ajomuch92ToDoContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=sql.bsite.net\\MSSQL2016;Database=ajomuch92_to_do;User Id=ajomuch92_to_do;Password=Hol@Mundo3192;Encrypt=False;");
+        => optionsBuilder.UseSqlServer();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
